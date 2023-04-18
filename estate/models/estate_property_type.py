0cea505c0600756de24b92402138c84046207724
cindypsd <cindy@vauxoo.com>
2023-04-18 13:08:15 -0600

    Initializing repository

--- estate/models/estate_property_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class PropertyType(models.Model):
-    _name = "property.type"
-    _description = "Property type model"
-
-   
-    name = fields.Char(string = 'Name', required=True)
-    property_ids = fields.One2many("property", "property_type_id", "Properties")
