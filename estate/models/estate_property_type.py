11b6f6d0ec69640c82ce292b8c65d63c08433ea5
Cindy Posada <cindypsd@Cindys-MacBook-Air.local>
2023-04-21 13:29:45 -0500

    [IMP] gitignore: Add folders and files, recover deleted files after rebase

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,5 @@ class PropertyType(models.Model):
     This is a model that represents different types of properties, with the name of each property type as the primary field. The model is designed to be used in conjunction with the "property" model, as it has a one-to-many relationship with "property" to link each property with its respective property type.
     """
 
-
     name = fields.Char(required=True)
     property_ids = fields.One2many("property", "property_type_id", "Properties")
